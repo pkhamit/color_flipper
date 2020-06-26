7773e15f8a3d26c12de4faade6c55254b83385dc
Punitha Krishnasamy <punitha.krishnasamy@avanade.com>
2020-06-26 23:29:31 +0200

    added hexcolors

--- hex.js
+++ hex.js
@@ -1,1 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const colortxt = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+let hexcolor = "#";
+for(var i = 0 ; i< 6 ; i++){
+ hexcolor += hex[Math.floor(Math.random() * hex.length)];
+}
+document.body.style.backgroundColor = hexcolor;
+colortxt.textContent = hexcolor;
+
+})
